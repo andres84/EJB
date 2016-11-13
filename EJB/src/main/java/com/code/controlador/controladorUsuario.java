2f6d3371830c39e3500dcd372e3f20a1b17d21c5
sil <sil@sil>
2016-11-13 08:06:18 -0500

    EJB version 2 Maven -creacion de paquetes con las clases(entity, ejb, managedbeanControlador) -creacion de la unidad de persistencia

---
+++ EJB/src/main/java/com/code/controlador/controladorUsuario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.code.controlador;
+
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author sil
+ */
+@Named(value = "controladorUsuario")
+@ViewScoped
+public class controladorUsuario implements Serializable{
+
+    /**
+     * Creates a new instance of controladorUsuario
+     */
+    public controladorUsuario() {
+    }
+    
+}
